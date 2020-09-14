d54da70c7b77a31f2d78ae73584fb75f08bf7418
JLormine <70603400+JLormine@users.noreply.github.com>
2020-09-14 16:47:17 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -3,8 +3,9 @@ def maine():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[3])
+   last = 13
+   rnd = random.randit(0,last)
+  print(quotes[rnd])
 
 if __name__== "__maine__":
   maine()
